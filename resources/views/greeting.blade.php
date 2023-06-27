b95d999feb657a6fbaf9d5c7a92e230914cd954b
thawatchaid123 <137174640+thawatchaid123@users.noreply.github.com>
2023-06-27 16:03:09 +0700

    27 มิย.

---
+++ resources/views/greeting.blade.php
@@ -0,0 +1,8 @@
+<!-- View stored in resources/views/greeting.blade.php -->
+<html>
+<body>
+<h1>Hello, {{ $name }} </h1>
+<h1>Hello, <?php echo $name; ?> </h1>
+</body>
+</html>
+
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get("/homepage", function() {
+    return "<h1>This is home page</h1>";
+   });
+   
+Route::get("/blog/{id}", function($id) {
+    return "<h1>This is blog page : {$id} </h1>" ;
+});
+Route::get("/hello", function () {	
+    return view("hello");
+    });
+
+
+// routes/web.php 
+Route::get('/greeting', function () {
+
+	$name = 'tom';
+$last_name = 'kuy';
+
+return view('greeting', compact('name','last_name') );
+});
+   
